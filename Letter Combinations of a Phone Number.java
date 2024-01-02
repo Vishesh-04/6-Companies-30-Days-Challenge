a146c310e4c09a0b36182391d3f06570b7230d0b
Vishesh-04 <visheshjindal04@gmail.com>
2024-01-02 20:13:37 +0530

    Letter Combinations of a Phone Number

--- Letter Combinations of a Phone Number.java
+++ Letter Combinations of a Phone Number.java
@@ -5,8 +5,8 @@ Given a string containing digits from 2-9 inclusive, return all possible letter 
 A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters. **/
 
 import java.util.*;
-public class Letter_Combinations_of_a_Phone_Number {
-    String [] arr = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+class Letter_Combinations_of_a_Phone_Number {
+    private static String [] arr = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public static List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
         if(digits.length()==0){
@@ -31,5 +31,7 @@ public class Letter_Combinations_of_a_Phone_Number {
         Scanner sc = new Scanner(System.in);
         String digits = sc.nextLine();
         List<String> res = letterCombinations(digits);
+        System.out.println("The Answer is :- "+res);
+        sc.close();
     }
 }
